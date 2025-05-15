ab83572acff71643e1bc35f9204ad6f630ae9f84
Morgan <m.smock973@gmail.com>
2025-05-15 11:03:46 +0200

    ajout des modification

---
+++ Components/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorApp1.Components.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Components.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
